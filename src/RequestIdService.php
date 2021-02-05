4a2584432b39a2633b2d49faf260b7cafbf5d565
natepage <nathan.page@eonx.com>
2021-02-05 05:54:30 +0000

    [EasyStandard] Implement AvoidPrivatePropertiesSniff (#466)
    Co-authored-by: Jay Are Galinada <73857985+jayaregalinadaflexisourceit@users.noreply.github.com>
    Co-authored-by: Dmitry Kapenkin <dkapenkin@rambler.ru>
    Co-authored-by: Roman <roman.ovchinnikov@eonx.com>
    Co-authored-by: Nathan Page <nathan.page@eonx.com>
    Co-authored-by: natepage <nathan.page86@gmail.com>

--- src/RequestIdService.php
+++ src/RequestIdService.php
@@ -102,6 +102,6 @@ final class RequestIdService implements RequestIdServiceInterface
      */
     private function filterResolvers(iterable $resolvers, string $class): array
     {
-        return CollectorHelper::orderLowerPriorityFirstAsArray(CollectorHelper::filterByClass($resolvers, $class));
+        return CollectorHelper::orderLowerPriorityFirst(CollectorHelper::filterByClass($resolvers, $class));
     }
 }
